214b45dcb665f89e0a27dfa74b1f7abad73e1910
klemenStanic <ks4211@student.uni-lj.si>
2021-01-05 19:59:02 +0100

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    // Here we put all the code
+    var heart = $('.heart'),
+        cog = $('#cog'),
+        popUp = $('.popUp'),
+        closePopUp = $('#closePopUp'),
+        cancelPopUp = $('#cancelPopUp');
+
+    heart.click(function(){
+        $(this).toggleClass('fa-heart-o');
+        $(this).toggleClass('heart-red fa-heart');
+    })
+
+    cog.click(function(){
+        popUp.fadeIn(500);
+    })
+
+    closePopUp.click(function(){
+        popUp.fadeOut(500);
+    })
+
+    cancelPopUp.click(function(){
+        popUp.slideUp(500)
+    })
+
+    
+
+
+})
+
+
+
+
+const log_me_in = async () => {
+    const url = "http://40.88.248.138:80/users-api/v1/user"; // site that doesn’t send Access-Control-*
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE) {
+            let response_json = JSON.parse(this.responseText);
+            console.log(response_json);
+            if (response_json.msg == "Login successful") {
+                document.cookie = response_json.token;
+                window.location.href = 'feed.html';
+            }
+        }
+    };
+
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send(JSON.stringify({
+        "email": email,
+        "password": password
+    }));
+}
+
+const register = async () => {
+
+    const url = "http://40.88.248.138:80/users-api/v1/register"; 
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        console.log("SMO TOLE");
+        if (this.readyState == XMLHttpRequest.DONE) {
+            let response_json = JSON.parse(this.responseText);
+            console.log(response_json);
+            console.log("asdasdsad");
+            if (response_json.msg == "Created new user.") {
+                document.cookie = response_json.token;
+                window.location.href = 'feed.html';
+            }
+        }
+    };
+
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send(JSON.stringify({
+        "email": email,
+        "password": password
+    }));
+}
+
---
+++ src/js/feed.js
@@ -0,0 +1,49 @@
+const get_me_feed = async () => {
+    const url = "http://40.88.248.138:80/feed-api/v1/feed"; // site that doesn’t send Access-Control-*
+    
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE) {
+            let response_json = JSON.parse(this.responseText);
+            console.log(response_json);
+            populate_feed(response_json["feed"]);
+        }
+    };
+
+
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', document.cookie);
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send();
+}
+
+var itm = document.getElementById("photo");
+itm.style.display = "none";
+
+
+get_me_feed();
+
+
+function populate_feed(feed){
+    console.log(feed);
+    var itm = document.getElementById("photo");
+    itm.style.display = "none";
+
+    for (const el of feed){
+        console.log(el);
+        var cln = itm.cloneNode(true);
+        cln.style.display = "inline";
+        cln.getElementsByClassName("photo__file")[0].src = el.path;
+        photo.src = el.path;
+
+        cln.getElementsByClassName("photo__username")[0].innerHTML = el.user_info.email;
+        cln.getElementsByClassName("photo__location")[0].innerHTML = el.title;
+        cln.getElementsByClassName("photo__time-ago")[0].innerHTML = el.created_on;
+
+        document.getElementById("feed").appendChild(cln);    
+    }
+    
+
+} 
